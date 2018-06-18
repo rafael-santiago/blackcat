b57c20b719b4af4a86f13f29cc4ebc964f50178a
rafael-santiago <voidbrainvoid@gmail.com>
2018-06-18 09:32:18 -0800

    Adding 'blackcat_clear_options_tests' test case

--- src/cmd/test/main.c
+++ src/cmd/test/main.c
@@ -10,12 +10,21 @@
 #include <cmd/version.h>
 #include <string.h>
 
+// INFO(Rafael): The test case 'blackcat_clear_option_tests' needs the following options
+//               out from the .rodata otherwise it would cause an abnormal program termination.
+
+static char path[] = "";
+static char cmd[] = "meow";
+static char arg2[] = "--foo=bar";
+static char arg3[] = "--bar=foo";
+static char arg4[] = "--bool";
+
 static char *argv[] = {
-    "",
-    "meow",
-    "--foo=bar",
-    "--bar=foo",
-    "--bool"
+    path,
+    cmd,
+    arg2,
+    arg3,
+    arg4
 };
 
 static int argc = sizeof(argv) / sizeof(argv[0]);
@@ -28,6 +37,7 @@ CUTE_DECLARE_TEST_CASE(blackcat_get_option_tests);
 CUTE_DECLARE_TEST_CASE(blackcat_get_bool_option_tests);
 CUTE_DECLARE_TEST_CASE(blackcat_get_argv_tests);
 CUTE_DECLARE_TEST_CASE(get_blackcat_version_tests);
+CUTE_DECLARE_TEST_CASE(blackcat_clear_options_tests);
 
 CUTE_MAIN(blackcat_cmd_tests_entry);
 
@@ -37,6 +47,7 @@ CUTE_TEST_CASE(blackcat_cmd_tests_entry)
     CUTE_RUN_TEST(blackcat_get_option_tests);
     CUTE_RUN_TEST(blackcat_get_bool_option_tests);
     CUTE_RUN_TEST(blackcat_get_argv_tests);
+    CUTE_RUN_TEST(blackcat_clear_options_tests);
     CUTE_RUN_TEST(get_blackcat_version_tests);
 CUTE_TEST_CASE_END
 
@@ -94,3 +105,14 @@ CUTE_TEST_CASE_END
 CUTE_TEST_CASE(get_blackcat_version_tests)
     CUTE_ASSERT(strcmp(get_blackcat_version(), "0.0.1") == 0);
 CUTE_TEST_CASE_END
+
+CUTE_TEST_CASE(blackcat_clear_options_tests)
+    int a;
+
+    blackcat_clear_options();
+
+    for (a = 0; a < argc; a++) {
+        CUTE_ASSERT(strlen(argv[a]) == 0);
+    }
+CUTE_TEST_CASE_END
+
