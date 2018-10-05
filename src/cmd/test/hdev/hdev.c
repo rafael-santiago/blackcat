41a49ccbe5f0f0ff99ef74e515aac465c1ca4488
Rafael Santiago <voidbrainvoid@gmail.com>
2018-10-05 23:09:48 +0200

    Just hooking a little more during the system test

--- src/cmd/test/hdev/hdev.c
+++ src/cmd/test/hdev/hdev.c
@@ -57,11 +57,20 @@ module_exit(hook_finis);
 #include <sys/sysproto.h>
 #include <kook.h>
 
-void *sys_write_p = NULL, *sys_read_p = NULL;
+void *sys_write_p = NULL, *sys_writev_p = NULL, *sys_pwrite_p = NULL,
+     *sys_read_p = NULL, *sys_readv_p = NULL, *sys_pread_p = NULL;
 
 static int hook_write(struct thread *td, void *args);
 
+static int hook_writev(struct thread *td, void *args);
+
+static int hook_pwrite(struct thread *td, void *args);
+
 static int hook_read(struct thread *td, void *args);
+
+static int hook_readv(struct thread *td, void *args);
+
+static int hook_pread(struct thread *td, void *args);
 
 static int ld(struct module *mod, int cmd, void *arg);
 
@@ -79,12 +88,20 @@ static int ld(struct module *mod, int cmd, void *arg) {
     switch (cmd) {
         case MOD_LOAD:
             kook(SYS_read, hook_read, &sys_read_p);
+            kook(SYS_readv, hook_readv, &sys_readv_p);
+            kook(SYS_pread, hook_pread, &sys_pread_p);
             kook(SYS_write, hook_write, &sys_write_p);
+            kook(SYS_writev, hook_writev, &sys_writev_p);
+            kook(SYS_pwrite, hook_pwrite, &sys_pwrite_p);
             break;
 
         case MOD_UNLOAD:
             kook(SYS_read, sys_read_p, NULL);
+            kook(SYS_readv, sys_readv_p, NULL);
+            kook(SYS_pread, sys_pread_p, NULL);
             kook(SYS_write, sys_write_p, NULL);
+            kook(SYS_writev, sys_writev_p, NULL);
+            kook(SYS_pwrite, sys_pwrite_p, NULL);
             break;
 
         default:
@@ -99,8 +116,24 @@ static int hook_write(struct thread *td, void *args) {
     return sys_write(td, args);
 }
 
+static int hook_writev(struct thread *td, void *args) {
+    return sys_writev(td, args);
+}
+
+static int hook_pwrite(struct thread *td, void *args) {
+    return sys_pwrite(td, args);
+}
+
 static int hook_read(struct thread *td, void *args) {
     return sys_read(td, args);
+}
+
+static int hook_readv(struct thread *td, void *args) {
+    return sys_readv(td, args);
+}
+
+static int hook_pread(struct thread *td, void *args) {
+    return sys_pread(td, args);
 }
 
 #elif defined(__NetBSD__)
