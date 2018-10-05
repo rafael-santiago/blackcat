e1cbb05cbed0bd5764bd937d5c62bc81df004f91
Rafael Santiago <voidbrainvoid@gmail.com>
2018-10-05 23:00:06 +0200

    Also verifying hooks for 'readv', 'pread', 'writev' and 'pwrite'

--- src/dev/freebsd/scan_hook.c
+++ src/dev/freebsd/scan_hook.c
@@ -14,7 +14,11 @@
 int scan_hook(void) {
     // TODO(Rafael): Find a way of verifying the sysent pointer in order to mitigate memory patching attacks.
     if (sysent[SYS_read].sy_call != (sy_call_t *)sys_read ||
+        sysent[SYS_readv].sy_call != (sy_call_t *)sys_readv ||
+        sysent[SYS_pread].sy_call != (sy_call_t *)sys_pread ||
         sysent[SYS_write].sy_call != (sy_call_t *)sys_write ||
+        sysent[SYS_writev].sy_call != (sy_call_t *)sys_writev ||
+        sysent[SYS_pwrite].sy_call != (sy_call_t *)sys_pwrite ||
         sysent[SYS_ioctl].sy_call != (sy_call_t *)sys_ioctl) {
         return 1;
     }
