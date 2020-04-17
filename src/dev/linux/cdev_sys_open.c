e6e54528850846a70b2a5cf1f7106aa3df491dc7
rafael-santiago <voidbrainvoid@tutanota.com>
2020-04-17 12:58:13 -0800

    Debugging avoidance fine tunings (Linux)

--- src/dev/linux/cdev_sys_open.c
+++ src/dev/linux/cdev_sys_open.c
@@ -12,7 +12,10 @@
 #include <linux/uaccess.h>
 
 #define has_blackcat_ref(s, se) ( (se) > (s) && (se)[0] == 'b' && (se)[1] == 'l' && (se)[2] == 'a' && (se)[3] == 'c' &&\
-                                                (se)[4] == 'k' && (se)[5] == 'c' && (se)[6] == 'a' && (se)[7] == 't')
+                                                (se)[4] == 'k' && (se)[5] == 'c' && (se)[6] == 'a' && (se)[7] == 't' )
+
+#define has_blackcat_dev_ref(s, se) ( ((se) - 4) > (s) && (se)[-4] == 'd' && (se)[-3] == 'e' && (se)[-2] == 'v' &&\
+                                                          (se)[-1] == '/' )
 
 asmlinkage long (*native_sys_open)(const char __user *, int, mode_t) = NULL;
 
@@ -21,8 +24,6 @@ asmlinkage long cdev_sys_open(const char __user *file, int flags, mode_t mode) {
     char *file_path = NULL;
     size_t file_path_size;
     long fd = -1;
-
-    printk(KERN_INFO "/dev/blackcat: cdev_sys.open() begin.\n");
 
     if (native_sys_open == NULL || file == NULL) {
         fd = -EFAULT;
@@ -50,12 +51,9 @@ asmlinkage long cdev_sys_open(const char __user *file, int flags, mode_t mode) {
 
         fp_end = file_path + file_path_size - 8;
 
-        if (has_blackcat_ref(file_path, fp_end)) {
-            printk(KERN_INFO "/dev/blackcat: cdev_sys.open() block.\n");
+        if (has_blackcat_ref(file_path, fp_end) && !has_blackcat_dev_ref(file_path, fp_end)) {
             fd = -EACCES;
             goto cdev_sys_open_epilogue;
-        } else {
-            printk(KERN_INFO "/dev/blackcat: cdev_sys.open() allowed.\n");
         }
     }
 
