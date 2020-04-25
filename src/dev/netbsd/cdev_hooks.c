a2650c6116e21a8f54dfd4de74730a06618c4410
rafael-santiago <voidbrainvoid@tutanota.com>
2020-04-25 13:25:58 -0800

    Updating cdev_hooks improvements for NetBSD

--- src/dev/netbsd/cdev_hooks.c
+++ src/dev/netbsd/cdev_hooks.c
@@ -14,56 +14,49 @@
 #define has_blackcat_dev_ref(s, se) ( ((se) - 4) > (s) && (se)[-4] == 'd' && (se)[-3] == 'e' && (se)[-2] == 'v' &&\
                                                           (se)[-1] == '/' )
 
-int (*native_sys_open)(struct thread *td, void *args) = NULL;
+int (*native_sys_open)(struct thread *td, struct open_args *uap) = NULL;
 
-int (*native_sys_openat)(struct thread *td, void *args) = NULL;
+int (*native_sys_openat)(struct thread *td, struct openat_args *uap) = NULL;
 
-int (*native_sys_rename)(struct thread *td, void *args) = NULL;
+int (*native_sys_rename)(struct thread *td, struct rename_args *uap) = NULL;
 
-int (*native_sys_renameat)(struct thread *td, void *args) = NULL;
+int (*native_sys_renameat)(struct thread *td, struct renameat_args *uap) = NULL;
 
-int (*native_sys_unlink)(struct thread *td, void *args) = NULL;
+int (*native_sys_unlink)(struct thread *td, struct unlink_args *uap) = NULL;
 
-int (*native_sys_unlinkat)(struct thread *td, void *args) = NULL;
+int (*native_sys_unlinkat)(struct thread *td, struct unlinkat_args *uap) = NULL;
 
 static int deny_path_access(const char *filepath);
 
-int cdev_sys_open(struct thread *td, void *args) {
-    struct open_args *uap;
+int cdev_sys_open(struct thread *td, struct open_args *uap) {
     int err = EACCES;
 
-    if ((err = native_sys_open(td, args)) == 0) {
-        uap = (struct open_args *)args;
-        if (deny_path_access(uap->path) && (uap->flags & (O_WRONLY|O_RDWR)) != 0) {
-            err = EACCES;
-        }
+    if (deny_path_access(uap->path) && (uap->flags & (O_WRONLY|O_RDWR)) != 0) {
+        err = EACCES;
+    } else {
+        err = native_sys_open(td, uap);
     }
 
     return err;
 }
 
-int cdev_sys_openat(struct thread *td, void *args) {
-    struct openat_args *uap;
+int cdev_sys_openat(struct thread *td, struct openat_args *uap) {
     int err = EACCES;
 
-    if ((err = native_sys_openat(td, args)) == 0) {
-        uap = (struct openat_args *)args;
-        if (deny_path_access(uap->path) && (uap->flag & (O_WRONLY|O_RDWR)) != 0) {
-            err = EACCES;
-        }
+    if (deny_path_access(uap->path) && (uap->flag & (O_WRONLY|O_RDWR)) != 0) {
+        err = EACCES;
+    } else {
+        err = native_sys_openat(td, uap);
     }
 
     return err;
 }
 
-int cdev_sys_rename(struct thread *td, void *args) {
-    struct rename_args *uap;
+int cdev_sys_rename(struct thread *td, struct rename_args *uap) {
     int err = EACCES;
-
-    uap = (struct rename_args *)args;
 
     if (!deny_path_access(uap->from) && !deny_path_access(uap->to)) {
-        err = native_sys_rename(td, args);
+        err = native_sys_rename(td, uap);
     } else {
         td->td_retval[0] = -1;
     }
@@ -71,14 +64,11 @@ int cdev_sys_rename(struct thread *td, void *args) {
     return err;
 }
 
-int cdev_sys_renameat(struct thread *td, void *args) {
-    struct renameat_args *uap;
+int cdev_sys_renameat(struct thread *td, struct renameat_args *uap) {
     int err = EACCES;
-
-    uap = (struct renameat_args *)args;
 
     if (!deny_path_access(uap->old) && !deny_path_access(uap->new)) {
-        err = native_sys_renameat(td, args);
+        err = native_sys_renameat(td, uap);
     } else {
         td->td_retval[0] = -1;
     }
@@ -86,14 +76,11 @@ int cdev_sys_renameat(struct thread *td, void *args) {
     return err;
 }
 
-int cdev_sys_unlink(struct thread *td, void *args) {
-    struct unlink_args *uap;
+int cdev_sys_unlink(struct thread *td, struct unlink_args *uap) {
     int err = EACCES;
 
-    uap = (struct unlink_args *)args;
-
     if (!deny_path_access(uap->path)) {
-        err = native_sys_unlink(td, args);
+        err = native_sys_unlink(td, uap);
     } else {
         td->td_retval[0] = -1;
     }
@@ -101,14 +88,11 @@ int cdev_sys_unlink(struct thread *td, void *args) {
     return err;
 }
 
-int cdev_sys_unlinkat(struct thread *td, void *args) {
-    struct unlinkat_args *uap;
+int cdev_sys_unlinkat(struct thread *td, struct unlinkat_args *uap) {
     int err = EACCES;
 
-    uap = (struct unlinkat_args *)args;
-
     if (!deny_path_access(uap->path)) {
-        err = native_sys_unlinkat(td, args);
+        err = native_sys_unlinkat(td, uap);
     } else {
         td->td_retval[0] = -1;
     }
@@ -118,32 +102,19 @@ int cdev_sys_unlinkat(struct thread *td, void *args) {
 
 static int deny_path_access(const char *filepath) {
     int deny = 1;
-    char *kfilepath = NULL;
-    size_t kfilepath_size;
+    size_t temp_size;
+    char temp[MAXPATHLEN];
 
-    if (filepath == NULL) {
+    if (filepath == NULL || copyinstr(filepath, temp, MAXPATHLEN - 1, NULL) != 0) {
         goto deny_path_access_epilogue;
     }
 
-    kfilepath_size = strlen(filepath);
+    temp_size = strlen(temp);
 
-    kfilepath = (char *) malloc(kfilepath_size + 1, M_TEMP, M_NOWAIT);
-
-    if (kfilepath == NULL) {
-        goto deny_path_access_epilogue;
-    }
-
-    memset(kfilepath, 0, kfilepath_size);
-    copyin(filepath, kfilepath, kfilepath_size);
-
-    deny = has_blackcat_ref(kfilepath, kfilepath + kfilepath_size - 8) &&
-           !has_blackcat_dev_ref(kfilepath, kfilepath + kfilepath_size - 8);
+    deny = has_blackcat_ref(temp, temp + temp_size - 8) &&
+           !has_blackcat_dev_ref(temp, temp + temp_size - 8);
 
 deny_path_access_epilogue:
-
-    if (kfilepath != NULL) {
-        free(kfilepath, M_TEMP);
-    }
 
     return deny;
 }
